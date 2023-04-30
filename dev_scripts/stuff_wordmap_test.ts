559231cff2c7721c8790a8b37c6c389cd0199645
Joshua Lansford <Joshua@Lansfords.com>
2023-04-30 12:16:35 -0400

    First pass of data for predict.  Doesn't have the location information yet.

--- dev_scripts/stuff_wordmap_test.ts
+++ dev_scripts/stuff_wordmap_test.ts
@@ -4,6 +4,9 @@ import * as fs from "fs-extra";
 //import WordMap from "../";
 import WordMap from "wordmap";
 import { Alignment, Ngram } from 'wordmap';
+import * as AdmZip from 'adm-zip';
+//import usfmjs from 'usfm-js';
+const usfmjs = require('usfm-js');
 
 // import * as map_without_alignment from './map_without_alignment';
 // import * as map_with_catboost from './map_with_catboost';
@@ -14,21 +17,21 @@ import { Alignment, Ngram } from 'wordmap';
 function load_book(folderPath: string): Map<number, any> {
     const bookJsonMap: Map<number, any> = new Map();
     for (let i = 1; ; i++) {
-      const filePath = `${folderPath}/${i}.json`;
-      if (!fs.existsSync(filePath)) {
-        break;
-      }
-      const jsonString = fs.readFileSync(filePath, "utf8");
-      const bookJson = JSON.parse(jsonString);
-      bookJsonMap.set(i, bookJson);
+        const filePath = `${folderPath}/${i}.json`;
+        if (!fs.existsSync(filePath)) {
+            break;
+        }
+        const jsonString = fs.readFileSync(filePath, "utf8");
+        const bookJson = JSON.parse(jsonString);
+        bookJsonMap.set(i, bookJson);
     }
     return bookJsonMap;
-  }
+}
   
 
 class ChapterVerse {
     constructor(public chapter: number, public verse: number) {}
-  }
+}
   
   
 function alignment_frequency_to_string( map: WordMap ){
@@ -78,6 +81,73 @@ function add_book_alignment_to_wordmap(excluded_verse: ChapterVerse, targetBook:
 }
 
 
+interface JsonDict {
+    [key: string]: any;
+}
+
+function load_json_zip(zip_path: string): JsonDict {
+    const zip = new AdmZip(zip_path);
+    const entries = zip.getEntries().filter(e => e.entryName.endsWith('.json'));
+    const data: JsonDict = {};
+
+    function load_json(file_data: Buffer): any {
+        const file_content = file_data.toString('utf-8');
+        return JSON.parse(file_content);
+    }
+
+    function stick_into_structure(dict: JsonDict, dict_path: string, content: any): void {
+        const parts = dict_path.split('/');
+        let current_dict = dict;
+        for (let i = 0; i < parts.length; i++) {
+            const part = parts[i];
+            if (i === parts.length - 1) {
+                current_dict[part] = content;
+            } else {
+                if (!(part in current_dict)) {
+                    current_dict[part] = {};
+                }
+                current_dict = current_dict[part];
+            }
+        }
+    }
+
+    for (const entry of entries) {
+        const file_data = zip.readFile(entry.entryName);
+        const file_name = entry.entryName.replace(/\.json$/, '');
+        stick_into_structure(data, file_name, load_json(file_data));
+    }
+
+    return data;
+}
+
+function normalizeVerseText( verse_text_in: string ): string{
+    const cleaned = usfmjs.removeMarker(verse_text_in);
+    const targetTokens = Lexer.tokenize(cleaned);
+    const normalizedTargetVerseText = targetTokens.map(t=>t.toString()).join(' ');
+    return normalizedTargetVerseText;
+}
+
+
+function compile_verse_text_pair(
+    greek_book: JsonDict,
+    loaded_book: Map<number, any>,
+    selected_verse: ChapterVerse
+): { sourceVerseText: any, targetVerseText: any } {
+    const sourceVerseObjects = greek_book[selected_verse.chapter][selected_verse.verse].verseObjects.filter(
+        (verseObj: any) => verseObj.type === "word"
+    );
+  
+    const targetVerseText = loaded_book.get(selected_verse.chapter)?.[selected_verse.verse];
+    const normalizedTargetVerseText = normalizeVerseText( targetVerseText );
+  
+    return { sourceVerseText: sourceVerseObjects, targetVerseText: normalizedTargetVerseText };
+}
+
+
+function saveJson(obj: object, path: string): void {
+    const json = JSON.stringify(obj, null, 2);
+    fs.writeFileSync(path, json);
+}
   
 
 if (require.main === module) {
@@ -99,4 +169,12 @@ if (require.main === module) {
         if (err) throw err;
         console.log('Data saved to file!');
     });
+
+
+    const greek_bible = load_json_zip( "/home/lansford/work2/Mission_Mutual/translationCore/tcResources/el-x-koine/bibles/ugnt/v0.30_Door43-Catalog/books.zip" )
+
+    //now see if we can produce the same sourceVerseText and targetVerseText.
+    const verseTextPair = compile_verse_text_pair( greek_bible.mat, loaded_book, new ChapterVerse(1, 1) );
+
+    saveJson( verseTextPair, "./dev_scripts/data/tokenized_verse_data2.json" );
 }
